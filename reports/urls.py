c37fac0c19b9569e7365de1b3f1da1292981ab07
savan <bharvadiyasavan96@gmail.com>
2026-08-25 08:37:51 +0530

    Task 8:Dashboard redirection

--- reports/urls.py
+++ reports/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     ),
     path('admins/', views.admin_list, name='admin_list'),
     path('students/', views.student_list, name='student_list'),
+    path('dashboard/', views.dashboard,name='dashboard'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Admin, Student
 
 @login_required
+def dashboard   (request):
+
+    if request.user.is_staff:
+        return redirect('admin_list')
+
+    return redirect('student_list')
+
+@login_required
 def admin_list(request):
     admins = Admin.objects.all()
-    return render(request, 'reports/admin_list.html', {'admins': admins})
+
+    return render(request,'reports/admin_list.html',{'admins': admins})
 
 @login_required
 def student_list(request):
     students = Student.objects.all()
-    return render(request, 'reports/student_list.html', {'students': students})
+
+    return render(request,'reports/student_list.html',{'students': students})
